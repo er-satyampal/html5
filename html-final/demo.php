6650ca6cd307117e6282bff0a76666a1b7feec27
unknown <em.satyam@gmail.com>
2023-01-24 19:08:55 +0530

    Minor File Chnages #240123

---
+++ html-final/demo.php
@@ -0,0 +1,22 @@
+<!-- <html>
+    <head>
+    </head>
+    <body>
+    <?php
+     echo "This is Human!";
+    ?>
+
+    </body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo ("My first PHP script!");
+?>
+
+</body>
+</html>
